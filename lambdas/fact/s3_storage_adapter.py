e6d82542e6cd24658c539ba375ae28860062f417
Juan Herrera <cripto1989@gmail.com>
2026-07-03 19:47:28 +0000

    Create the S3StorageAdapter

--- lambdas/fact/s3_storage_adapter.py
+++ lambdas/fact/s3_storage_adapter.py
@@ -1,16 +1,14 @@
-from typing import Dict
-
 import awswrangler as wr
 import pandas as pd
 from storage_port import StoragePort
 
 
 class S3StorageAdapter(StoragePort):
-    def read_parquet(self, event: Dict) -> pd.DataFrame:
-        s3_bucket_path = event.get("s3_bucket_path")
-        date = event.get("date")
-        raw_data_path = f"{s3_bucket_path}raw/events/raw_events_{date}.jsonl"
-        return wr.s3.read_json(raw_data_path, lines=True)
+    def read_json(self, path: str) -> pd.DataFrame:
+        return pd.read_json(path, lines=True)
+
+    def read_parquet(self, path: str) -> pd.DataFrame:
+        return wr.s3.read_parquet(path)
 
     def write_parquet(self):
         pass
